a43701d518b72db25d9b3492519747dab24a47eb
worace <horace.d.williams@gmail.com>
2015-09-28 19:27:13 -0600

    horace and brett initial hamt attempt

--- src/hamt/core.clj
+++ src/hamt/core.clj
@@ -1,6 +1,58 @@
 (ns hamt.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn bits [num]
+  (Integer/toBinaryString num))
+
+;; make an empty hamt
+;; assoc into the trie
+;; get key from trie
+
+;; data -stores data
+;; children -- 0-31 vec with slots for subtries
+
+;;{:data nil :children []}
+(defn hamt
+  ([] {:key nil :value nil :children (vec (repeat 31 nil))})
+  ([key val] {:key key :value val :children (vec (repeat 31 nil))}))
+
+;; {:children [{:key nil :value nil :children []} {...}]}
+
+(defn h-assoc [trie key value & [hash-digits]]
+  (if (nil? (:key trie))
+    (assoc trie :key key :value value)
+    (let [hash-digits (or hash-digits (hash key))
+          i (bit-and 31 hash-digits)
+          subtrie (get-in trie [:children i])]
+      (if subtrie
+        (assoc-in
+         trie [:children i]
+         (h-assoc subtrie
+                  key
+                  value
+                  (bit-shift-right hash-digits 5)))
+        (assoc-in
+         trie
+         [:children i]
+         (hamt key value))))))
+
+;; hashcodes -> positions in the children list
+;; 32 children in each level, 5 bits of hashcode
+;; to ID a position
+;;
+(defn h-get [trie key]
+  (if (= key (:key trie))
+    (:value trie)
+    (let [h (hash key)
+          i (bit-and 31 h)
+          subtrie (get-in trie [:children i])]
+      (if subtrie
+        (h-get subtrie key)
+        nil))))
+
+
+(def compact (partial filter (comp not nil?)))
+(defn trie-string [t]
+  (if t
+    (str "Key: " (:key t) " Val: " (:value t) " Sub: \n    "
+         (clojure.string/join "\n  " (map trie-string (compact (:children t)))))
+    ""))
--- test/hamt/core_test.clj
+++ test/hamt/core_test.clj
@@ -2,6 +2,47 @@
   (:require [clojure.test :refer :all]
             [hamt.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-new-trie
+  (testing "hamt gives empty trie"
+    (is (nil? (:key (hamt))))
+    (is (nil? (:value (hamt))))))
+
+(deftest test-assoc-empty-trie
+  (testing "adding data to empty trie"
+    (is (= :pizza (:key (h-assoc (hamt) :pizza :pie))))))
+
+(deftest test-assoc-positions
+  (testing "stickes em in right index"
+    (let [t (-> (hamt)
+                (h-assoc :pizza :pie)
+                (h-assoc :calzone :pocket))]
+      (is (= :calzone (get-in t [:children 21 :key])))
+      (is (= :pocket (get-in t [:children 21 :value]))))))
+
+(deftest test-assoc-second-value
+  (testing "nests deeper in the trie"
+    (let [t (-> (hamt)
+                (h-assoc :pizza :pie)
+                (h-assoc :calzone :pocket))]
+      (is (= :pocket (h-get t :calzone))))))
+
+
+(deftest test-assoc-many-things
+  (testing "make it break"
+    (let [t (-> (hamt)
+                (h-assoc :pizza :pie)
+                (h-assoc :calzone :pocket)
+                (h-assoc :stromboli :roll)
+                (h-assoc :linguine :strings)
+                (h-assoc :lasagna :ruffles))]
+      (is (= :strings (h-get t :linguine))))))
+
+(deftest test-appending-lower-levels
+  (testing "uses next 5 bits of hash code"
+    (let [t (-> (hamt)
+                (h-assoc :pizza :pie)
+                (h-assoc :calzone :pocket)
+                ;; 23 has same first 5 bits as calzone
+                ;; and different second 5
+                (h-assoc 23 "random"))]
+      (is (= 23 (get-in t [:children 21 :children 6 :key]))))))
